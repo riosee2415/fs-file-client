07661e98dd3d87542f8a88584732e497cbfc93cf
4leaf.ysh <4leaf.ysh@gmail.com>
2020-07-13 22:31:05 +0900

    class-2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
+  componentDidMount = async () => {
+    const data1 = "세종대왕님";
+    const data2 = "하이!";
+
+    const inputData = {
+      data1: data1,
+      data2: data2,
+    };
+
+    await axios.post(
+      "/api/test",
+      {
+        params: { inputData },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
+
   render() {
     return <div>Hello React</div>;
   }
