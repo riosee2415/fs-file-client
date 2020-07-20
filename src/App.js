4662cd137d6bc183d4d41d7213814747d2f429e5
4leaf.ysh <4leaf.ysh@gmail.com>
2020-07-20 21:45:14 +0900

    ac

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { memo } from "react";
 import axios from "axios";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      memoData: null,
+    };
+  }
+
   componentDidMount = async () => {
     const data1 = "세종대왕님";
     const data2 = "하이!";
@@ -11,21 +19,43 @@ class App extends React.Component {
       data2: data2,
     };
 
-    await axios.post(
-      "/api/test",
-      {
-        params: { inputData },
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
+    await axios
+      .post(
+        "/api/test",
+        {
+          params: { inputData },
         },
-      }
-    );
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) =>
+        this.setState({
+          memoData: response.data,
+        })
+      );
   };
 
   render() {
-    return <div>Hello React</div>;
+    const { memoData } = this.state;
+
+    return (
+      <div>
+        {memoData === null
+          ? "Loading..."
+          : memoData.map((memo) => {
+              return (
+                <div key={memo.refKey}>
+                  <h1>{memo.title}</h1>
+                  <h3>{memo.content}</h3>
+                  <h4>{memo.regDate}</h4>
+                </div>
+              );
+            })}
+      </div>
+    );
   }
 }
 
